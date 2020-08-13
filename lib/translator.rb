1bae9be255ba357e448f2122efb19e253a1e1ac3
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 14:18:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,10 +12,9 @@ def load_library(file_path)
 new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon()
   # code goes here
-  emoticon = load_library(file_path)
-  binding.pry
+  
   
 end
 
