25ba98fad3cdbbcc3d6ce3d006e226662ac1010c
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 14:44:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,6 +29,8 @@ def get_english_meaning(file_path, emoticon)
   load_library(file_path)
   old_data = YAML.load_file(file_path)
   emoticon_pair = old_data.values
+  emoticon_pair.map do |pair|
+end
   binding.pry
   
 end
