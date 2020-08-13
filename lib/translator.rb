bf3528253d7403d1f3ad4cc7583fac46041f9ae9
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 14:11:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,15 @@
 # require modules here
-
-def load_library
+require 'yaml'
+require 'pry'
+def load_library(file_path)
   # code goes here
+  old_data = YAML.load_file(file_path)
+  new_hash = {}
+  old_data.map do |emotion, faces|
+    
+    new_hash[emotion] = {:english => faces[0], :japanese => faces[1]}
+  end
+new_hash
 end
 
 def get_japanese_emoticon
