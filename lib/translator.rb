d0d20c3ef0b8f6e425a6c8013df63a91c9406527
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 14:39:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
     return pair[1]
    end
  end
-   puts "Sorry that is not an emoticon"
+   return "Sorry that is not an emoticon"
 end
 
 def get_english_meaning
